1341c0514f05320fe5bee0fac24614edd01c96b5
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-03 14:57:46 +0530

    Add solution for Leaders in an Array - TUFHub

--- DSA/Arrays/leaders-in-an-array/solution.java
+++ DSA/Arrays/leaders-in-an-array/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<Integer> leaders(int[] nums) {
         int n = nums.length;
-        List<Integer> ans = new ArrayList<>();
-        ans.add(nums[n-1]);
-        int max = nums[n-1];
-        for(int i = n-2; i >=0;i--){
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[n - 1]);
+        int max = nums[n - 1];
+        for(int i = n - 2; i >=0;i--){
             if(nums[i] > max){
                 max = nums[i];
-                ans.add(nums[i]);
+                list.add(nums[i]);
             }
         }
-        Collections.reverse(ans);
-        return ans;
+        Collections.reverse(list);
+        return list;
     }
 }
