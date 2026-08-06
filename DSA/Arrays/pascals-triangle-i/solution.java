fa569e310c8a1978f4cc30bd5a03a92644fff276
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-06 15:24:04 +0530

    Add solution for Pascal's Triangle I - TUFHub

--- DSA/Arrays/pascals-triangle-i/solution.java
+++ DSA/Arrays/pascals-triangle-i/solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public int pascalTriangleI(int r, int c) {
-        return nCr(r - 1, c - 1);
+        return nCr(r - 1, c- 1);
     }
-    int nCr(int n, int r){
-        if(r > n-r){
-            r = n-r;
-        }
+    int nCr (int n, int r){
+        if(r > n - r) r = n - r;
         if(r == 1) return n;
-        
         int result = 1;
+
         for(int i = 0; i < r; i++){
             result *= (n - i);
             result /= (i + 1);
