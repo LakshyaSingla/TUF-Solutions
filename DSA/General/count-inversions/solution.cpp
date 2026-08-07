e8fac26bc599c2e6a5126771c4fce2bbd7d62dfd
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-08 00:23:19 +0530

    Add solution for Count Inversions - TUFHub

---
+++ DSA/General/count-inversions/solution.cpp
@@ -0,0 +1,11 @@
+            }else{
+                temp.add(nums[i]);
+                i++;
+        List<Integer> temp = new ArrayList<>();
+        while(i <= mid && j <= high){
+            if(nums[i] <= nums[j]){
+        long count = 0;
+    long merge(int[] nums, int low ,int mid, int high){
+        int i = low;
+        int j = mid + 1;
+class Solution {
