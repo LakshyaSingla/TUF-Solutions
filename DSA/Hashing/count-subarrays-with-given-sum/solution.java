72c99d43b0e6fd0bfe392a7aff43c34a6def46d7
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-16 22:10:19 +0530

    Sync Count subarrays with given sum [DSA] - TUFHub

---
+++ DSA/Hashing/count-subarrays-with-given-sum/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int n = nums.length;
+        int sum = 0;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            if(sum == k){
+                count++;
+            }
+            int rem = sum - k;
+            if(mpp.containsKey(rem)){
+                count += mpp.get(rem);
+            }
+            mpp.put(sum, mpp.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
