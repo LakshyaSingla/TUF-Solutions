8ca887d80ddbe73eb01c33055496f272332cbc54
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-24 21:48:26 +0530

    Sync Rat in a Maze [DSA] - TUFHub

--- DSA/Backtracking/rat-in-a-maze/solution.java
+++ DSA/Backtracking/rat-in-a-maze/solution.java
@@ -1,26 +1,26 @@
 class Solution {
-    void path(int[][] grid, int i, int j, List<String> ans, String curr, int n){
-        if(i == n - 1 && j == n - 1){
-            ans.add(curr);
+    void move(int i, int j, int[][] grid, String dir,List<String> ans){
+        if(i == grid.length - 1 && j == grid.length - 1){
+            ans.add(dir);
             return;
         }
         if(grid[i][j] == 0) return;
         grid[i][j] = 0;
-
-        if(i > 0) path(grid, i - 1, j, ans, curr + 'U', n);
-        if(j > 0) path(grid, i, j - 1, ans, curr + 'L', n);
-        if(i < n -1) path(grid, i + 1, j, ans, curr + 'D', n);
-        if(j < n-1) path(grid, i, j + 1, ans, curr + 'R', n);
+        if(i > 0)move(i-1,j,grid,dir+'U',ans);
+        if(i<grid.length - 1)move(i+1,j,grid,dir+'D',ans);
+       if(j>0) move(i,j-1,grid,dir+'L',ans);
+        if(j<grid.length -1 )move(i, j+1, grid, dir + 'R', ans);
         grid[i][j] = 1;
+
     }
     public List<String> findPath(int[][] grid) {
         //your code goes here
         int n = grid.length;
         List<String> ans = new ArrayList<>();
-        if(grid[0][0] == 0 || grid[n-1][n-1] == 0){
-            return ans;
-        }
-        path(grid, 0, 0, ans, "", n);
+        if(grid[0][0] == 0 || grid[n-1][n-1] == 0) return ans;
+
+        move(0,0,grid,"", ans);
         return ans;
+
     }
 }
