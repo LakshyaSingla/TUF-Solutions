509cd7912e3b17727261a4eae0634c8e5ee9b290
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-26 12:34:47 +0530

    Sync M Coloring Problem [DSA] - TUFHub

--- DSA/Backtracking/m-coloring-problem/solution.java
+++ DSA/Backtracking/m-coloring-problem/solution.java
@@ -1,26 +1,28 @@
 class Solution {
     boolean isSafe(int color, int node, int[] colors, List<List<Integer>> adj){
-
+        
         for(int neighbors : adj.get(node)){
             if(colors[neighbors] == color) return false;
         }
         return true;
     }
-    boolean AssignColors(int node, int m, int n, int[] colors, List<List<Integer>> adj){
-
+    boolean AssignColors(int node, int m, int n,int[] colors, List<List<Integer>> adj){
         if(node == n) return true;
+        
+
         for(int i = 1; i <= m; i++){
-            if(isSafe(i, node, colors, adj)){
-                colors[node] = i;
-                if(AssignColors(node + 1, m, n, colors, adj)) return true;
-                colors[node] = 0;
+            if(isSafe(i, node, colors, adj )){
+            colors[node] = i;
+            if(AssignColors(node + 1, m,n,colors,adj)) return true;
+            colors[node] = 0;
             }
+            
         }
         return false;
     }
     boolean graphColoring(int[][] edges, int m, int n) {
         //your code goes here
-        List<List<Integer>> adj = new ArrayList<>();
+         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
