badc69871d302532ee93f5bd9cf53c22a12f968f
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-27 14:56:37 +0530

    Sync Job sequencing Problem [DSA] - TUFHub

--- DSA/Greedy/job-sequencing-problem/solution.java
+++ DSA/Greedy/job-sequencing-problem/solution.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] JobScheduling(int[][] Jobs) {
         //your code goes here
-        Arrays.sort(Jobs, (a, b) -> b[2] - a[2]);
-        int maxDeadline = 0;
+        Arrays.sort(Jobs, (a, b) -> Integer.compare(b[2], a[2]));
+        int maxDeadline = -1;
+        for(int[] it : Jobs){
+            maxDeadline = Math.max(maxDeadline, it[1]);
+        }
+        int[] deadline = new int[maxDeadline];
+        Arrays.fill(deadline, -1);
+        int count = 0, profit = 0;
         for(int[] job : Jobs){
-            maxDeadline = Math.max(maxDeadline, job[1]);
-        }
-        int[] deadlineDays = new int[maxDeadline];
-        Arrays.fill(deadlineDays, -1);
-        int num = 0, profit = 0;
-        for(int i = 0; i < Jobs.length; i++){
-            
-            for(int j = Jobs[i][1] - 1; j >= 0; j--){
-                if(deadlineDays[j] == -1){
-                    deadlineDays[j] = Jobs[i][0];
-                    profit += Jobs[i][2];
-                    num++;
-                    break;
-                }
+            for(int j = job[1] - 1; j >= 0; j-- ){
+                if(deadline[j] == -1){
+                count++;
+                deadline[j] = job[0];
+                profit += job[2];
+                break;
             }
+            }
+            
         }
-        return new int[]{num, profit};
+        return new int[]{count, profit};
     }
 }
