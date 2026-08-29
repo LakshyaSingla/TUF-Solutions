891203e936718250f6bf3e77ab72a9ef687a21b0
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-29 20:02:56 +0530

    Sync M Coloring Problem [DSA] - TUFHub

--- DSA/Backtracking/m-coloring-problem/solution.java
+++ DSA/Backtracking/m-coloring-problem/solution.java
@@ -1,37 +1,35 @@
 class Solution {
-    boolean isSafe(int color, int node, int[] colors, List<List<Integer>> adj){
-        
-        for(int neighbors : adj.get(node)){
-            if(colors[neighbors] == color) return false;
+    boolean isValid(int color, int node, List<List<Integer>> adj, int[] colors){
+        for(int neighbour : adj.get(node)){
+            if(colors[neighbour] == color) return false;
         }
         return true;
     }
-    boolean AssignColors(int node, int m, int n,int[] colors, List<List<Integer>> adj){
-        if(node == n) return true;
-        
-
+    boolean backtrack(int node, int n, int m, List<List<Integer>> adj, int[] colors){
+        if(n == node){
+            return true;
+        }
         for(int i = 1; i <= m; i++){
-            if(isSafe(i, node, colors, adj )){
-            colors[node] = i;
-            if(AssignColors(node + 1, m,n,colors,adj)) return true;
-            colors[node] = 0;
+            if(isValid(i, node, adj, colors)){
+                colors[node] = i;
+                if(backtrack(node + 1, n,m,adj,colors)) return true;
+                colors[node] = 0;
             }
-            
         }
         return false;
     }
     boolean graphColoring(int[][] edges, int m, int n) {
         //your code goes here
-         List<List<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
-
         for(int[] edge : edges){
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
         int[] colors = new int[n];
-        return AssignColors(0, m , n,colors,adj);
+       return backtrack(0, n, m, adj, colors);
+
     }
 }
