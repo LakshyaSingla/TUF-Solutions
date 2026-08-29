1c4a4a44e386b7062a3e2906b7ce94d32b6779b5
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-29 20:06:42 +0530

    Sync Shortest Job First [DSA] - TUFHub

--- DSA/Greedy/shortest-job-first/solution.java
+++ DSA/Greedy/shortest-job-first/solution.java
@@ -2,13 +2,11 @@ class Solution {
     public long solve(int[] bt) {
         //your code goes here
         Arrays.sort(bt);
-        int n = bt.length;
-        long waittime = 0, totaltime = 0;
-        for(int i = 0; i < n; i++){
-            waittime += totaltime;
-            totaltime += bt[i];
-            
+        int waitTime = 0, totalTime = 0;
+        for(int i = 0; i < bt.length; i++){
+            waitTime += totalTime;
+            totalTime += bt[i];
         }
-        return waittime / n ;
+        return waitTime / bt.length;
     }
 }
