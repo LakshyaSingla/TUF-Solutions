7a425d6881d014555575ba59cd9a9cce98909f3d
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-02 12:05:46 +0530

    Sync Longest Repeating Character Replacement [DSA] - TUFHub

--- DSA/Sliding-Window/longest-repeating-character-replacement/solution.java
+++ DSA/Sliding-Window/longest-repeating-character-replacement/solution.java
@@ -2,20 +2,18 @@ class Solution {
     public int characterReplacement(String s, int k) {
         //your code goes here
         int n = s.length();
-        int l = 0, r = 0, maxlen = 0, maxfreq = 0;
         int[] hash = new int[26];
-
+        int l = 0, r = 0, maxlen = 0, maxfreq = 0;
         while(r < n){
             hash[s.charAt(r) - 'A']++;
             maxfreq = Math.max(maxfreq, hash[s.charAt(r) - 'A']);
-            int change = (r - l + 1) - maxfreq;
-            if(change > k){
-                hash[s.charAt(l) - 'A']--;
+
+            if((r - l + 1) - maxfreq > k){
+                hash[s.charAt(l) -'A']--;
                 l++;
             }
-            if(change <= k){
-                maxlen = Math.max(maxlen, r - l + 1);
-            }
+            
+            maxlen = Math.max(maxlen, r - l + 1);
             r++;
         }
         return maxlen;
