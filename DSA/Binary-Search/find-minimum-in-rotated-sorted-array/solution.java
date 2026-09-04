957bf18790f7527a651ebfb0aeb7bc7235ecd1d2
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-05 00:16:32 +0530

    Sync Find minimum in Rotated Sorted Array [DSA] - TUFHub

---
+++ DSA/Binary-Search/find-minimum-in-rotated-sorted-array/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMin(ArrayList<Integer> arr) {
+      int n = arr.size();
+
+      int low = 0, high = n - 1, ans = Integer.MAX_VALUE;
+
+      while(low <= high){
+        int mid = low + (high - low) / 2;
+
+        if(arr.get(low) <= arr.get(high)){
+            ans = Math.min(ans, arr.get(low));
+        }
+        if(arr.get(mid) >= arr.get(low)){
+            ans = Math.min(ans, arr.get(low));
+            low = mid + 1;
+        }else{
+            ans = Math.min(ans, arr.get(mid));
+            high = mid - 1;
+        }
+      }
+      return ans;
+    }
+}
