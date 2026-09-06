bd72c1aa68c19cb2e07be9c6950b1fea48352869
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-06 12:59:55 +0530

    Sync Find Nth root of a number [DSA] - TUFHub

--- DSA/Binary-Search/find-nth-root-of-a-number/solution.java
+++ DSA/Binary-Search/find-nth-root-of-a-number/solution.java
@@ -1,27 +1,32 @@
 class Solution {
     int PoW(int mid, int N, int M){
-        long ans = 1, base = mid;
+        long base = mid;
+        long ans = 1;
         while(N > 0){
             if(N % 2 == 1){
-                ans *= base;
-                N--;
-                if(ans > M) return 2;
-            }else{
-                base*= base;
-                N /= 2;
-                if(base > M) return 2;
-            }
+            ans *= base;
+            if(ans > M) return 2;
+            N--;
+        }else if(N % 2 == 0){
+            base *= base;
+            N/=2;
+            if(base > M) return 2;
+        }
         }
         if(ans == M) return 1;
         return 0;
+
+        
+
     }
     public int NthRoot(int N, int M) {
         int low = 1, high = M;
+
         while(low <= high){
             int mid = low + (high - low) / 2;
-            int midN = PoW(mid, N, M);
-            if(midN == 1) return mid;
-            else if(midN == 0){
+            if(PoW(mid, N, M) == 1){
+                return mid;
+            }else if(PoW(mid, N, M) == 0){
                 low = mid + 1;
             }else{
                 high = mid - 1;
