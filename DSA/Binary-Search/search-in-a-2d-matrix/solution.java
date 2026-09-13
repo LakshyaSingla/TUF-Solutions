160fbc28037ef1909546aea26e194d1d90ac826c
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-14 00:10:11 +0530

    Sync Search in a 2D matrix [DSA] - TUFHub

--- DSA/Binary-Search/search-in-a-2d-matrix/solution.java
+++ DSA/Binary-Search/search-in-a-2d-matrix/solution.java
@@ -1,32 +1,20 @@
 class Solution {
-    boolean binarySearch(int[] row, int target){
-        int low = 0, high = row.length - 1;
+    public boolean searchMatrix(int[][] mat, int target) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int low = 0, high = m*n - 1;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(row[mid] == target){
+            int row = mid / n;
+            int col = mid % n;
+            if(mat[row][col] == target){
                 return true;
-            }else if(row[mid] < target){
+            }else if(mat[row][col] < target){
                 low = mid + 1;
             }else{
                 high = mid - 1;
             }
         }
         return false;
-    }
-    public boolean searchMatrix(int[][] mat, int target) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int low = 0, high = n - 1, ansRow = -1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(mat[mid][m - 1] < target){
-                low = mid + 1;
-            }else{
-                ansRow = mid;
-                high = mid - 1;
-            }
-        }
-        if(ansRow == -1) return false;
-        return binarySearch(mat[ansRow], target);
     }
 }
