9b48d58e4c2bcc15c4c72c67b5fc071032f5cd9d
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-24 23:38:37 +0530

    Sync Next Greater Element [DSA] (TUFHub)

---
+++ DSA/Stack-Queue/next-greater-element/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextLargerElement(int[] arr) {
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i = arr.length - 1; i >= 0; i--){
+
+            while(!st.isEmpty() && arr[i] >= st.peek()){
+                st.pop();
+            }
+            if(st.isEmpty()) ans[i] = -1;
+            else ans[i] = st.peek();
+            st.push(arr[i]);
+
+        }
+        return ans;
+    }
+}
